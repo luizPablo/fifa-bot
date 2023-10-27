e6e1f97145c68773a9501efb6c960703d6961d76
Luiz Pablo <lpablotavares@gmail.com>
2023-10-26 21:43:27 -0300

    browser set

--- index.js
+++ index.js
@@ -15,45 +15,44 @@ const extractMatch = (msg) => {
     return splitedMessage[1];
 }
 
-(async () => {
-    const client = new Client({
-        puppeteer: {
-            executablePath: '/usr/bin/chromium-browser',
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--enable-gpu', '--disable-extensions'],
+const client = new Client({
+    puppeteer: {
+        executablePath: '/usr/bin/chromium-browser',
+        ignoreHTTPSErrors: true,
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--enable-gpu', '--disable-extensions'],
+    }
+});
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    const isMatchMessage = isMatchPostMessage(msg);
+
+    if (msg.body === '!test') {
+        msg.reply('I am alive!');
+    }
+
+    if (isMatchMessage) {
+        const match = extractMatch(msg);
+
+        if (!match) {
+            msg.reply('Erro ao registrar o jogo. Tente novamente. Ex: !jogo Flamengo 2-1 River');
+        } else {
+            msg.reply(`## *${match}* ## registrado com sucesso! Você tem *12h* para postar o resultado.`);
+
+            setTimeout(() => {
+                msg.reply(`## *${match}* ## 12h se passaram. Cadastrou o resultado?`);
+            }, 43200000);
         }
-    });
+    }
+});
 
-    client.on('qr', (qr) => {
-        console.log('QR RECEIVED', qr);
-        qrcode.generate(qr, { small: true });
-    });
-
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
-
-    client.on('message', msg => {
-        const isMatchMessage = isMatchPostMessage(msg);
-
-        if (msg.body === '!test') {
-            msg.reply('I am alive!');
-        }
-
-        if (isMatchMessage) {
-            const match = extractMatch(msg);
-
-            if (!match) {
-                msg.reply('Erro ao registrar o jogo. Tente novamente. Ex: !jogo Flamengo 2-1 River');
-            } else {
-                msg.reply(`## *${match}* ## registrado com sucesso! Você tem *12h* para postar o resultado.`);
-
-                setTimeout(() => {
-                    msg.reply(`## *${match}* ## 12h se passaram. Cadastrou o resultado?`);
-                }, 43200000);
-            }
-        }
-    });
-
-    client.initialize();
-})();
+client.initialize();
