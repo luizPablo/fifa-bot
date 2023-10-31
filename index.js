4f4ddf0c11da777a5b68331eaaf2bb5f4de1beb2
Luiz Pablo <lpablotavares@gmail.com>
2023-10-31 12:51:52 -0300

    fix args

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const {
 const client = new Client({
     puppeteer: {
         executablePath: '/usr/bin/chromium-browser',
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--enable-gpu', '--disable-extensions'],
     }
 });
 
