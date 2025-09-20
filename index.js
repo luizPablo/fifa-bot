12d6b4492a7acf2e1861fcf16ea0e57c011ec302
Luiz Pablo <lpablotavares@gmail.com>
2025-09-20 12:44:00 -0300

    bugfix: fix client issues

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const client = new Client({
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
             '--no-zygote',
-            '--single-process', // <- this one
+            '--single-process',
             '--disable-gpu'
         ],
     }
